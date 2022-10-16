ad772708cbef4e535de1476e465037eee19ef232
rolnd30 <114603660+rolnd30@users.noreply.github.com>
2022-10-16 08:43:06 -0600

    Vistas producto

---
+++ resources/views/tarea/create.blade.php
@@ -0,0 +1,21 @@
+@extends('tema.app')
+
+@section('title','Nuevo')
+
+@section('contenido')
+    <h3>
+        Registrar producto
+    </h3>
+    <form action="{{ route('tarea.store') }}" method="POST">
+        <x-tarea-form-body/>
+    </form>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+@endsection
---
+++ resources/views/tarea/index.blade.php
@@ -0,0 +1,10 @@
+@extends('tema.app')
+
+@section('title','Listado')
+
+@section('contenido')
+    <h3>
+        Listado de productos
+    </h3>
+    @livewire('tarea-index')
+@endsection
