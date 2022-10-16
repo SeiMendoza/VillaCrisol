cacb95b49a923f2d597fbcf2f060abf4fc428a13
rolnd30 <114603660+rolnd30@users.noreply.github.com>
2022-10-16 08:03:06 -0600

    Modelo producto

---
+++ app/Models/Tarea.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Tarea extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = "tareas";
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'perece',
+        'categoria',
+        'observacion'
+    ];
+
+    public $timestamps = false;
+
+    public const PERECE = ['No', 'Si'];
+
+    public function perece(){
+        return self::PERECE[$this->perece];
+    }
+}
+
