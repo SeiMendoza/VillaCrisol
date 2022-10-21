bd8b2d1efa285581a2e1a6af4a2db948587adbc8
Ejlopez10 <93482799+Ejlopez10@users.noreply.github.com>
2022-10-21 15:27:50 -0600

    Update DetallesEmpleado.blade.php

--- resources/views/Empleados/DetallesEmpleado.blade.php
+++ resources/views/Empleados/DetallesEmpleado.blade.php
@@ -1,67 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Detalles de: {{$empleado-> NombreCompleto }}</title>
-</head>
-<body>
-    <br><br>
-<h1 class="text-center">Detalles de: {{$empleado-> NombreCompleto }}<br></h1>
-<div class="container">
-    <div class="col-sm12 col-md-12 col-lg-9">
-<table class="table">
-    <thead class="thead-light ">
-        <tr> 
-            <th scope="col">Datos</th>
-            <th scope="col">Información</th>
-            <th scope=""><a class="btn btn-warning" href="{{route('empleado.editar', ['id'=>$empleado->id])}}">Editar</a></th>
-        </tr>
-    </thead>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>Detalles de: {{$empleado-> NombreCompleto }}</title>
+    </head>
     <body>
-        
-        <tr>
-        <th scope="row">Nombre Completo:</th>
-        <td>{{$empleado->NombreCompleto}}</td>
-        </tr>
-        <tr>
-        <th scope="row">Número De Identidad:</th>
-        <td>{{$empleado->NúmeroDeIdentidad}}</td>
-    </tr>
-    <tr>
-        <th scope="row">Correo Electrónico:</th>
-        <td>{{$empleado->CorreoElectrónico}}</td>
-    </tr>
-    <tr>
-        <th scope="row">Telefono:</th>
-        <td>{{$empleado->NúmeroTelefónico}}</td>
-    </tr>
-    <tr>
-        <th scope="row">Nombre Contacto de la Empresa:</th>
-        <td>{{$empleado->NombreDeLaReferencia}}</td>
-    </tr>
-    <tr>
-        <th scope="row">Número Contacto de la Empresa:</th>
-        <td>{{$empleado->NúmeroDeReferencia}}</td>
-    </tr>
-    <tr>
-        <th scope="row">Domicilio:</th>
-        <td>{{$empleado->Domicilio}}</td>
-    </tr>
-    <tr>
-        <th scope="row">Fecha Ingreso:</th>
-        <td>{{$empleado->FechaDeIngreso}}</td>
-    </tr>
-    <tr>
-        <th scope="row">Estado del Empleado:</th>
-        <td>{{$empleado->Estado}}</td>
-        </tr>
-    </tbody>
-</table>
-</div>
+        <div class="container-fluid px-4">
+            
+            <br><br>
+            <div class="card shadow col-md-12 items-center">
+                <h1 class="text-center">Detalles de: {{$empleado-> NombreCompleto }}<br></h1>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="thead-light ">
+                                <tr>
+                                    <th scope="col">Datos</th>
+                                    <th scope="col">Información</th>
+                                    <th scope=""><a class="btn btn-warning" href="{{route('empleado.editar', ['id'=>$empleado->id])}}">Editar</a></th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-<a class='btn btn-primary' href="{{route('empleado.index')}}">volver</a>
-</div>
+                                <tr>
+                                    <th scope="row">Nombre Completo:</th>
+                                    <td>{{$empleado->NombreCompleto}}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Número De Identidad:</th>
+                                    <td>{{$empleado->NúmeroDeIdentidad}}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Correo Electrónico:</th>
+                                    <td>{{$empleado->CorreoElectrónico}}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Telefono:</th>
+                                    <td>{{$empleado->NúmeroTelefónico}}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Nombre Contacto de la Empresa:</th>
+                                    <td>{{$empleado->NombreDeLaReferencia}}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Número Contacto de la Empresa:</th>
+                                    <td>{{$empleado->NúmeroDeReferencia}}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Domicilio:</th>
+                                    <td>{{$empleado->Domicilio}}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Fecha Ingreso:</th>
+                                    <td>{{$empleado->FechaDeIngreso}}</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">Estado del Empleado:</th>
+                                    <td>{{$empleado->Estado}}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="card shadow col-md-12">
+            <div class="card-body" style="text-align: center">
+                <a class='btn btn-primary' href="{{route('empleado.index')}}">volver</a>
+            </div>
+        </div>
+        <br>
+        <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid px-4">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright &copy; CENTRO TURISTICO VILLACRISOL 2022</div>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>
