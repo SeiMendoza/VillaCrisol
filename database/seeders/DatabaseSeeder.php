204add71892f74e5b91d5594898897f64e0e91c6
SeiMendoza <seily.mendoza@unah.hn>
2022-10-27 20:05:02 -0600

    cambios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([EmpleadoSeeder::class,
-                    CompraSeeder::class,
                     ClienteSeeder::class,
                     ComidaBebidaSeeder::class,
                     ProductoSeeder::class]);
